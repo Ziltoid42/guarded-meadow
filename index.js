4c8611f0cdf2e064a740abb86d4865acbb24eb18
gcadel <gcadel@debian>
2016-10-27 13:16:20 +0200

    token tout seul

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200)
 })
 
-const token = "{EAANk8UeY4h4BAA8miLL5a5ZAD3tp9pkuhIQ4HnRVDh7ZAoXUPYj8G3XN4Q3aUg1TdJqOAhov4qJ0LQ6Dutq2zIo0PK9KCwtNryQ0HjFnZCDfhIvnvsmHB3xZC7DiMbBfr1vQXbdfi21UZB83NJb5L4dNtSrvXAE436g4TcgZCDbwZDZD}"
+const token = "EAANk8UeY4h4BAA8miLL5a5ZAD3tp9pkuhIQ4HnRVDh7ZAoXUPYj8G3XN4Q3aUg1TdJqOAhov4qJ0LQ6Dutq2zIo0PK9KCwtNryQ0HjFnZCDfhIvnvsmHB3xZC7DiMbBfr1vQXbdfi21UZB83NJb5L4dNtSrvXAE436g4TcgZCDbwZDZD"
 
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
