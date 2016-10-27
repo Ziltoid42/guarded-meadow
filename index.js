704c0e625dff9caeafeb971810e1bafddc205727
gcadel <gcadel@debian>
2016-10-27 12:15:00 +0200

    retry avec npm décent

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// index route
+app.get('/', function (req, res) {
+	res.send('hello world i am maripoza')
+})
+
+// for facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function(){
+	console.log('Running on port', app.get('port')	
+})
