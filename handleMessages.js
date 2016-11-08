adf3391b3643cc989bd3d8211a19ed6311ee8538
gcadel <gcadel@debian>
2016-11-08 09:56:41 +0100

    verification que ça passe bien dans handle

--- handleMessages.js
+++ handleMessages.js
@@ -8,13 +8,34 @@ module.exports = function (senderId, message) {
 
 
     if (messageText.toLowerCase() === 'test') {
-        var textReply = new fbMessage
+        /*var textReply = new fbMessage
             .PlainText("[DEBUG] SenderId: " + senderId + " Message JSON: " + JSON.stringify(message))
             .compose();
 
-        sendMessage(senderId, textReply);
+        sendMessage(senderId, textReply);*/
+        sendTextMessage(sender, event.sender.id);
     }
 
+
+    //fonction pour test
+    function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 
     //ok now here we can handle generic messages received by the bot...
