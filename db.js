9cea7b77456a8a202ce40b0ffde535b0f8b4e405
gcadel <gcadel@debian>
2016-11-12 11:09:46 +0100

    test de query

--- db.js
+++ db.js
@@ -13,7 +13,7 @@ var User = mongoose.model('User', {name: String, roles: Array, age: Number});
 /**
  * Lets Use our Models
  * */
-
+/*
 //Lets create a new user
 var user1 = new User({name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']});
 
@@ -30,6 +30,31 @@ user1.save(function (err, userObj) {
   } else {
     console.log('saved successfully:', userObj);
   }
+});*/
+
+//Lets try to Find a user
+User.findOne({name: 'modulus admin'}, function (err, userObj) {
+  if (err) {
+    console.log(err);
+  } else if (userObj) {
+    console.log('Found:', userObj);
+
+    //For demo purposes lets update the user on condition.
+    if (userObj.age != 30) {
+      //Some demo manipulation
+      userObj.age += 30;
+
+      //Lets save it
+      userObj.save(function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Updated', userObj);
+        }
+      });
+    }
+  } else {
+    console.log('User not found!');
+  }
 });
 
-module.exports = db;
