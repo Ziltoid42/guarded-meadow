933dc4c5f4b7839760be2b90af46beaf906c8bdb
gcadel <gcadel@debian>
2016-11-13 05:19:28 +0100

    remplacer id par objet user

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 //const fetch = require('node-fetch');
 const app = express();
-const db = require('./db');
+//const db = require('./db'); // de coté por test
 var handleMessages = require('./handleMessages');
 var handlePostbacks = require('./handlePostbacks');
 
