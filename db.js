8b02e01f712156ff7166be3a78ef0cc537e4d395
gcadel <gcadel@debian>
2016-11-14 08:01:59 +0100

    juste mongo sans export

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 //lets require/import the mongodb native drivers.
 var mongodb = require('mongodb');
-module.exports = function () {//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+db = (function () {//We need to work with "MongoClient" interface in order to connect to a mongodb server.
 var MongoClient = mongodb.MongoClient;
 var MONGOLAB_URI = 'greg:1186sousou@ds151707.mlab.com:51707';
 var uristring= 'mongodb://'+MONGOLAB_URI+'/heroku_x563fr8q';
@@ -21,5 +21,5 @@ MongoClient.connect(url, function (err, db) {
     db.close();
   }
 });
-
+})()
 module.exports = db
