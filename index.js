095f2cdc62f4fb60672e40a80f51d553359eaea3
gcadel <gcadel@debian>
2016-11-16 08:23:09 +0100

    test

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(app.get('port'), function(){
 
 //Zone function test promise//
 var user = {name: 'gregoun', fbid: 2132};
-db.findbyid(user);
+db.findfbid(user);
 /*
 var promise = new Promise(function(resolve, reject) {
   resolve(db.findbyid(user));
