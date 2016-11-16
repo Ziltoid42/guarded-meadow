cf99439e27374b0fafaddf94c6a59da02343990c
gcadel <gcadel@debian>
2016-11-16 10:54:23 +0100

    test

--- db.js
+++ db.js
@@ -79,10 +79,41 @@ function findtest(user){
     });
 }
 
+
 var user = {name: 'gregoun', fbid: 2132};
 var promise = new Promise(function(resolve, reject) {
-  resolve(findtest(user));
-});
+  //resolve(findtest(user));
+  MongoClient.connect(url, function (err, db) {
+      if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+      } else {
+        //HURRAY!! We are connected. :)
+        console.log('Connection established to', url);
+
+        // Get the documents collection
+        var collection = db.collection('users');
+
+        // Insert some users
+        collection.find({fbid: user.fbid}).toArray(function (err, result) {
+          if (err) {
+            reject(err);
+          } else if (result.length) {
+            //console.log('Dans findfbid:', result);
+          } else {
+            console.log('No document(s) found with defined "find" criteria!');
+          }
+          //Close connection
+          db.close();
+          resolve (result);
+          
+            });
+          //return copy; //.toArray(function(err, items){
+          //return items;
+            //})
+        });
+      }
+    });
+
 
 promise
 .then((result)=>{
