8142b72bd91f3bc32aec4ff8543125ec78f500c6
gcadel <gcadel@debian>
2016-11-18 09:16:50 +0100

    test comparaison timesamp

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function receivedDeliveryConfirmation(event) {
         messageID);
     });
   }
-
+  console.log("Timestamp perso: ", timestamp);
   console.log("All message before %d were delivered.", watermark);
 }
 //Fin zone test promise//
